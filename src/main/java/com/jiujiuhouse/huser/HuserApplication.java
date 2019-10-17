6cca6587f5770e69f187d22ec72fe6eed2ac3771
zzh <zzh@iquanwai.com>
2019-10-17 16:16:24 +0800

    fix

---
+++ src/main/java/com/jiujiuhouse/huser/HuserApplication.java
@@ -0,0 +1,13 @@
+package com.jiujiuhouse.huser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiujiuhouse/huser/HuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiujiuhouse.huser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
