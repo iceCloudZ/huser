0eb946ae658e487335f26284c1a033ae901c8636
zihao.zhao <zzh58@vip.qq.com>
2019-11-08 13:39:11 +0800

    修改测试类

--- src/test/java/com/jiujiuhouse/huser/HuserApplicationTests.java
+++ src/test/java/com/jiujiuhouse/huser/HuserApplicationTests.java
@@ -15,9 +15,6 @@ class HuserApplicationTests {
 
 	@Test
 	void contextLoads() {
-		final WxMaService wxService = WxMaConfiguration.getMaService(appid);
-		WxMaPhoneNumberInfo phoneNoInfo = wxService.getUserService().getPhoneNoInfo("8XXBVDMSOZmvzNyX3xrXOQ==", "pOIDVDM+T4fE1fuJG9OE1TeLnLvnCWun5/DtmKHyVOskiQk4DyDjLQg6Cy382gJNp9twY4afC1sqmaqps/1CbTE3sO/UEjHxqlyqJ72t+cNdE3neoB4+s/eiKLOapMjm68ZUhQ8VPjPTY/vWU7qHi8+UyafMN7Zfbvz9NfGalM9nnN1or52PXw+UOV0Rsgdrl2i+DKEieuUZtNQOCzY5hA==", "cMFM2hUcVkc1cZlnxXjyaA==");
-		System.out.println(phoneNoInfo.toString());
 
 	}
 
