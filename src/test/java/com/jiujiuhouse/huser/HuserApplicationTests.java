e8f3458b802d9ab4f8574a2c904dd88207cdc5e4
zihao.zhao <zzh58@vip.qq.com>
2019-11-08 20:15:30 +0800

    yml修改

--- src/test/java/com/jiujiuhouse/huser/HuserApplicationTests.java
+++ src/test/java/com/jiujiuhouse/huser/HuserApplicationTests.java
@@ -1,9 +1,5 @@
 package com.jiujiuhouse.huser;
 
-import cn.binarywang.wx.miniapp.api.WxMaService;
-import cn.binarywang.wx.miniapp.bean.WxMaPhoneNumberInfo;
-import com.jiujiuhouse.huser.wxmini.config.WxMaConfiguration;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,10 +8,5 @@ class HuserApplicationTests {
 
 	@Value("${wx.miniapp.configs[0].appid}")
 	private String appid;
-
-	@Test
-	void contextLoads() {
-
-	}
 
 }
