e03a497144c9dce403ec6dea970f900ccee37697
zihao.zhao <zzh58@vip.qq.com>
2019-11-08 15:25:07 +0800

    修改测试类

--- src/main/java/com/jiujiuhouse/huser/HeartBeatController.java
+++ src/main/java/com/jiujiuhouse/huser/HeartBeatController.java
@@ -4,13 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 @Slf4j
 public class HeartBeatController {
 
+    @PostConstruct
+    public void init(){
+        log.info("健康检查服务初始化.");
+    }
+
     @GetMapping("/health")
     public String HeartBeat() {
-        log.info("HeartBeating");
+
         return "health";
     }
 }
