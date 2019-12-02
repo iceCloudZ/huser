9a91ac2072f0c7a9b4a860d80cd880b0dedc123e
zihao.zhao <zzh58@vip.qq.com>
2019-12-02 20:34:42 +0800

    '熔断增加日志'

--- src/main/java/com/jiujiuhouse/huser/feign/hystrix/HresourceClientHystrix.java
+++ src/main/java/com/jiujiuhouse/huser/feign/hystrix/HresourceClientHystrix.java
@@ -2,10 +2,14 @@ package com.jiujiuhouse.huser.feign.hystrix;
 
 import com.alibaba.fastjson.JSONObject;
 import com.jiujiuhouse.huser.feign.HresourceClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 /**
  * @author zihao.zhao
  */
+@Slf4j
+@Component
 public class HresourceClientHystrix implements HresourceClient {
 
     @Override
