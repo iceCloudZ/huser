2336fc4a5d47d9e3e682df27b487462ae93385d0
lmw <13585973708@139.com>
2019-12-10 18:02:46 +0800

    添加了实体类

--- src/main/java/com/jiujiuhouse/huser/base/Result.java
+++ src/main/java/com/jiujiuhouse/huser/base/Result.java
@@ -14,14 +14,17 @@ public class Result<T> implements Serializable {
     public static String SUCCESS = "0";
 
     public static <T> Result<T> create(String code, String message) {
+
         return new Result(code, message, (Object) null);
     }
 
     public static <T> Result<T> create(String code, String message, T data) {
+
         return new Result(code, message, data);
     }
 
     public static <T> Result<T> successData(String message, T data) {
+
         return create(SUCCESS, message, data);
     }
 
@@ -34,38 +37,47 @@ public class Result<T> implements Serializable {
     }
 
     public static <T> Result<T> success() {
+
         return successMessage((String) null);
     }
 
     public static <T> Result<T> errorData(String message, T data) {
+
         return create(ERROR, message, data);
     }
 
     public static <T> Result<T> error() {
+
         return errorMessage((String) null);
     }
 
     public static <T> Result<T> errorData(T data) {
+
         return errorData((String) null, data);
     }
 
     public static <T> Result<T> errorMessage(String message) {
+
         return (Result<T>) errorData(message, (Object) null);
     }
 
     public static <T> Result<T> selectiveMessage(boolean success, String successMessage, String errorMessage) {
+
         return success ? successMessage(successMessage) : errorMessage(errorMessage);
     }
 
     public static <T> Result<T> selectiveMessage(boolean success, String successMessage, String errorMessage, T data) {
+
         return success ? successData(successMessage, data) : errorMessage(errorMessage);
     }
 
     public static <T> Result<T> error(Error e) {
+
         return e == null ? null : create(ERROR, e.getMessage());
     }
 
     public static <T> Result<T> error(Exception e) {
+
         return e == null ? null : create(ERROR, e.getMessage());
     }
 
@@ -90,26 +102,32 @@ public class Result<T> implements Serializable {
     }
 
     public String getCode() {
+
         return this.code;
     }
 
     public void setCode(String code) {
+
         this.code = code;
     }
 
     public T getData() {
+
         return this.data;
     }
 
     public void setData(T data) {
+
         this.data = data;
     }
 
     public String getMessage() {
+
         return this.message;
     }
 
     public void setMessage(String message) {
+
         this.message = StringUtils.isEmpty(message) ? "" : message;
     }
 
--- src/main/java/com/jiujiuhouse/huser/entity/ProfileRequirement.java
+++ src/main/java/com/jiujiuhouse/huser/entity/ProfileRequirement.java
@@ -12,16 +12,39 @@ import javax.persistence.*;
  * @author zihao.zhao
  */
 @Data
-@Entity
+@Entity//声明此类是个实体类
 @NoArgsConstructor
-@Table(name = "jj_profile_requirement")
+@Table(name = "jj_profile_requirement")//配置实体类和标的映射关系
 public class ProfileRequirement extends BaseEntity {
 
-    @Id
+    @Id//声明主键
     @SequenceGenerator(sequenceName = "profile_requirement_sequence", name = "seq")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")//主键的生成策略
     @Column(name = "id")
     private Integer id;
+
+    @Column(name = "cust_name",columnDefinition="varchar(10) not null")
+    private String custName;//客户姓名
+
+    @Column(name = "cust_phone",columnDefinition="varchar(11) not null")
+    private String custPhone;//客户电话
+
+    @Column(name = "address_name",columnDefinition="varchar(50) not null")
+    private String address;//地址
+
+    @Column(name = "address_real",columnDefinition="varchar(50) not null")
+    private String addressReal;//地址
+
+    @Column(name = "buildingFloorNum",columnDefinition="varchar(20)")
+    private String buildingFloorNum;
+
+    @Column(name = "rent",columnDefinition="varchar(20) not null")
+    private int rent;
+
+    @Column(name = "useArea",columnDefinition="varchar(20) not null")
+    private String useArea;
+
+
 
 
 
